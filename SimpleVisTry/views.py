26ad58df7fab23b2b80221d70926a68f349ead6f
Abhis33 <abhishek.mane33@gmail.com>
2020-04-25 21:22:29 -0700

    Top 5 Station map Done - Different javascript file to do and other type of visualization
    Top 5 Station map Done - Different javascript file to do
    - other type of visualization
    - Top 5 End Station?

--- SimpleVisTry/views.py
+++ SimpleVisTry/views.py
@@ -23,5 +23,39 @@ class subscriber_data(APIView):
             "default" : values,
             "subscriber_type" : subscriber_type
         }
+
+        print("")
         print(data)
         return Response(data)
+
+class top_k_start_stn(APIView):
+
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, format=None):
+        k = 5
+        stn_obj = CitiBike.objects.values("start_station_id","start_station_name","start_station_latitude","start_station_longitude").annotate(Count("id")).order_by('-id__count')[:k]
+
+        #Need these stuff to plot on map
+        #start_station_id = []
+        #start_station_name
+        #start_station_latitude
+        #start_station_longitude
+
+        ######################## GeoJson Conversion code
+        geo_json = [ {"type": "Feature",
+                    "properties": {
+                        "id":  each['start_station_id'],
+                        "name": each['start_station_name'],
+                        "Count": each['id__count'],
+                        "popupContent":  "id=%s" % (each['start_station_name'],)
+                        },
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [ each['start_station_longitude'],each['start_station_latitude'] ] }}
+                    for each in stn_obj ]
+
+        ####--------
+
+        return Response(geo_json)
